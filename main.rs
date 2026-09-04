619d4de0b2491933bceb98b0de8a9b999c6e173b
well-okay-then <ericsoppi@proton.me>
2026-09-04 17:39:06 -0400

    done

--- main.rs
+++ main.rs
@@ -1,25 +1,21 @@
-use std::io::{self, BufRead};
-
-mod geometry {
-    pub fn circle_area(dim: f64) -> f64 {
-        dim * dim * 3.14
-    }
-
-    pub fn square_area(dim: f64) -> f64 {
-        dim * dim
-    }
-}
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    let kind: String = lines.next().unwrap().unwrap();
-    let dim: f64 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let area = if kind.trim() == "circle" {
-        geometry::circle_area(dim)
-    } else {
-        geometry::square_area(dim)
-    };
-    println!("{:.2}", area);
+    for _ in 0..4 {
+        let counter = Arc::clone(&counter);
+        handles.push(thread::spawn(move || {
+            for _ in 1..=250 {
+                let mut c = counter.lock().unwrap();
+                *c += 1;
+            }
+        }));
+    }
+    for h in handles {
+        h.join().unwrap();
+    }
+    println!("{}", *counter.lock().unwrap());
 }
