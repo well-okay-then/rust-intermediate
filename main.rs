2e94ef36ce2f6fee0481997b2b99fe59fa2ca62c
well-okay-then <ericsoppi@proton.me>
2026-09-04 16:11:38 -0400

    done

--- main.rs
+++ main.rs
@@ -1,14 +1,19 @@
 use std::io::{self, BufRead};
 
-fn append_excl(s: &mut String) {
-    s.push('!');
+fn longer<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() >= b.len() {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     let stdin = io::stdin();
-    let mut line = String::new();
-    stdin.lock().read_line(&mut line).unwrap();
-    let mut s = line.trim().to_string();
-    append_excl(&mut s);
-    println!("{}", s);
+    let mut lines = stdin.lock().lines();
+    let a = lines.next().unwrap().unwrap();
+    let b = lines.next().unwrap().unwrap();
+    println!("{}", longer(&a, &b));
+    let _ = a;
+    let _ = b;
 }
