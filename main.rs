e6e309a9dc028c97c3ae560a66b285696de45bde
well-okay-then <ericsoppi@proton.me>
2026-09-04 15:39:44 -0400

    done

--- main.rs
+++ main.rs
@@ -1,12 +1,14 @@
 use std::io::{self, BufRead};
 
-// fn append_excl(s: &mut String) { ... }
+fn append_excl(s: &mut String) {
+    s.push('!');
+}
 
 fn main() {
     let stdin = io::stdin();
     let mut line = String::new();
     stdin.lock().read_line(&mut line).unwrap();
     let mut s = line.trim().to_string();
-    // append_excl(&mut s);
+    append_excl(&mut s);
     println!("{}", s);
 }
